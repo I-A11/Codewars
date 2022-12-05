cc01bc398a1a39e529ded3d47c141ea8c841bb16
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-05 19:21:46 +1100

    Solve 7 kyu codewars kata.

---
+++ string-ends-with.js
@@ -0,0 +1,11 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/javascript
+
+//Difficulty: 7 kyu
+
+//Solution
+
+const solution = (str, ending) => {
+  return str.endsWith(ending);
+};
