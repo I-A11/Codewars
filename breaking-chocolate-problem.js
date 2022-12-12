702c9e6c6a493a1a6bdb14c80e9e10b09047aedf
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-12 19:16:18 +1100

    Solve codewars kata.

---
+++ breaking-chocolate-problem.js
@@ -0,0 +1,10 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/534ea96ebb17181947000ada/solutions/javascript
+
+//Solution
+
+const breakChocolate = (n, m) => {
+  if (n < 1 || m < 1) return 0;
+  return n * m - 1;
+};
