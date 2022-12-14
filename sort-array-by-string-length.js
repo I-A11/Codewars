2b3205ed8eee0b77c690f24814275ddb2d0fea07
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-14 15:28:17 +1100

    Solve codewars kata.

---
+++ sort-array-by-string-length.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/javascript
+
+//Solution
+
+const sortByLength = (array) => {
+  return array.sort((a, b) => a.length - b.length);
+};
