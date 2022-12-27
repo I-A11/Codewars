4939cc40bf15a9c8a9893c8db9b1e9448a41b9c8
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-27 19:15:07 +1100

    Solve codewars kata.

---
+++ small-enough-Beginner.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/57cc981a58da9e302a000214/javascript
+
+//Solution
+
+const smallEnough = (a, limit) => {
+  return a.every((ele) => ele <= limit);
+};
