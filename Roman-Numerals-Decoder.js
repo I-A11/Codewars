c49e05eb154db8b5d686915de9ea7f5660f0a50b
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-28 11:39:04 +1100

    Solve codeWars kata.

---
+++ Roman-Numerals-Decoder.js
@@ -0,0 +1,23 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/51b6249c4612257ac0000005/solutions/javascript
+
+//Solution
+
+function solution(roman) {
+  var data = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
+  var numbers = roman.split("");
+  var sum = 0,
+    i;
+
+  for (i = 0; i < numbers.length; i++) {
+    if (data[numbers[i]] < data[numbers[i + 1]]) {
+      sum += data[numbers[i + 1]] - data[numbers[i]];
+      i++;
+    } else {
+      sum += data[numbers[i]];
+    }
+  }
+
+  return sum;
+}
