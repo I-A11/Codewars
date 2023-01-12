6d95bd1897d3a54cd9b1c7830dd0f4ff5405bdf5
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-01-12 15:36:54 +1100

    Solve codewars kata.

---
+++ JavaScript-Array-Filter.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/514a6336889283a3d2000001/javascript
+
+//Solution
+
+const getEvenNumbers = (numbersArray) => {
+  return numbersArray.filter((num) => num % 2 === 0);
+};
