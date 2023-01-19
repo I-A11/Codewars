c1fda9f6f51950d8f111bf19e4d2623137013504
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-01-20 10:45:34 +1100

    Solve codewars kata.

---
+++ No-oddities-here.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/javascript
+
+//Solution
+
+const noOdds = (values) => {
+  return values.filter((num) => num % 2 === 0);
+};
