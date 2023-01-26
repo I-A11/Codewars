c9d52d65510e8ec326ee7b6889b9594814dd7647
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-01-27 10:31:28 +1100

    Solve codewars kata.

---
+++ Number-of-Decimal-Digits.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+//  https://www.codewars.com/kata/58fa273ca6d84c158e000052/solutions/javascript
+
+//Solution
+
+const digits = (n) => {
+  return n.toString().split("").length;
+};
