faaf18a9a753ee0dc8e0a87ffcf264cb0bbebfca
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-02-04 14:28:50 +1100

    Solve codewars kata.

---
+++ Sort-the-Gift-Code.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/52aeb2f3ad0e952f560005d3/solutions/javascript
+
+//Solution
+
+const sortGiftCode = (code) => {
+  return code.split("").sort().join("");
+};
