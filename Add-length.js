21b56e438f9cd76a238637ed7cd6f52ed791b79f
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-02-18 22:56:55 +1100

    Solve add length codewars kata.

---
+++ Add-length.js
@@ -0,0 +1,11 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/559d2284b5bb6799e9000047/solutions/javascript
+
+//Solution
+
+const addLength = (str) => {
+  return str.split(" ").map((item) => {
+    return `${item} ${item.length}`;
+  });
+};
