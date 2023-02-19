32d120c857e3f1a491b270fdd4400bd370ed6656
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-02-19 11:50:52 +1100

    Solve vowel remover kata in codewars.

---
+++ Vowel-remover.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/5547929140907378f9000039/javascript
+
+//Solution
+
+const shortcut = (string) => {
+  return string.replace(/[aeiou]/g, "");
+};
