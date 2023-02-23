c9478a718f35bd141b06387c145a579e679e6b0c
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-02-23 13:34:16 +1100

    Solve codewars kata.

---
+++ Sort-by-Last-Char.js
@@ -0,0 +1,11 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0/javascript
+
+//Solution
+
+function last(x) {
+  return x
+    .split(" ")
+    .sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+}
