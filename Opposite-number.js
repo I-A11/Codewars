df714e3d080ba064bb7d028471af61c6840f0d5e
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-03-09 18:50:41 +1100

    Solve opposite number kata in codewars.

---
+++ Opposite-number.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/javascript
+
+//Solution
+
+const opposite = (number) => {
+  return number === 0 ? 0 : number * -1;
+};
