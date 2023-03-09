4781ed07fa156ac8943ef4dfea188c663b8e7ea9
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-03-09 19:07:18 +1100

    Use eval function to turn string into operation.

---
+++ Basic-Mathematical-Operations.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/57356c55867b9b7a60000bd7/solutions/javascript
+
+//Solution
+
+const basicOp = (operation, value1, value2) => {
+  return eval(value1 + operation + value2);
+};
