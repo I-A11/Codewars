51e7abcae52f5e973c7e91b91690a84afb3753e6
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-03-19 10:03:20 +1100

    Solve codewars kata.

---
+++ Build-a-square.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/javascript
+
+//Solution
+
+const generateShape = (n) => {
+  return ("+".repeat(n) + "\n").repeat(n).trim();
+};
