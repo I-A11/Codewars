19b054240201f4f192fde7b309d60015ef8d6e4e
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-03-20 17:39:57 +1100

    Solve codewars kata.

---
+++ double-the-values.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/53951fff369894e4f10007a9/javascript
+
+//Solution
+
+const double = (array) => {
+  return array.map((num) => num * 2);
+};
