3553e7845c16a37a5297aafde7e41a4d63f9e648
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-06-17 17:34:49 +1000

    Solve codeware kata use reduce method.

---
+++ sum-arrays.js
@@ -0,0 +1,9 @@
+//CodeWare link for the kyu:
+
+// https://www.codewars.com/kata/53dc54212259ed3d4f00071c/javascript
+
+//Solution
+
+const sum = (numbers) => {
+  return numbers.reduce((total, curr) => total + curr, 0);
+};
