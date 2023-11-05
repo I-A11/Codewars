2070bf36f63b7a5f2333aba88bd9ed6969b97efb
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-11-05 17:41:33 +1100

    Use .every to test if array includes all string char.

---
+++ Mutations.js
@@ -0,0 +1,17 @@
+Mutations;
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+// Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+// Solution
+const mutation = (arr) => {
+  const arr1 = arr.slice(0, 1).toString().toUpperCase().split("");
+
+  const arr2 = arr.slice(1).toString().toUpperCase().split("");
+
+  return arr2.every((element) => arr1.includes(element));
+};
