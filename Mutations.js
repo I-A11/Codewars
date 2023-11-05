7ad0538f851c5e559991d8390ec2b7e3b9a78b0d
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-11-05 17:45:14 +1100

    Comment the problem header.

--- Mutations.js
+++ Mutations.js
@@ -1,4 +1,4 @@
-Mutations;
+// Mutations;
 // Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
 
 // For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
