14d977b98edc22d6d0e0d1ae82ab4970cf1fcb86
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-04-22 10:45:59 +1000

    Use forEach method to loop and create ojbect from the array.

---
+++ ColourAssociation.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// Colour plays an important role in our lifes. Most of us like this colour better then another. User experience specialists believe that certain colours have certain psychological meanings for us.
+
+// You are given a 2D array, composed of a colour and its 'common' association in each array element. The function you will write needs to return the colour as 'key' and association as its 'value'.
+
+// For example:
+
+// var array = [["white", "goodness"], ...] //returns [{white: 'goodness'}, ...]
+
+//Solution
+
+const colourAssociation = (array) => {
+  let result = [];
+  array.forEach((entry) => {
+    let obj = {};
+    obj[entry[0]] = entry[1];
+    result.push(obj);
+  });
+  return result;
+};
