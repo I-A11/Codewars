0d515fe3dbe130fa1a8a69a064e95d9a66541cde
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2026-02-06 14:32:56 +1100

    Use for loop to create array of number multiples.

---
+++ Return-the-first-M-multiples-of-N.js
@@ -0,0 +1,16 @@
+// Description:
+// Implement a function that takes two numbers m and n and returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+// Ex.
+
+// (3, 5.0) --> [5.0, 10.0, 15.0]
+
+// Solution:
+
+const multiples = (m, n) => {
+  const result = [];
+  for (let i = 1; i <= m; i++) {
+    result.push(i * n);
+  }
+  return result;
+};
